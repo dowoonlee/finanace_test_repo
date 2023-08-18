524c50c3b54145d224a84a567c677dac17b248b7
dwlee717 <dwlee717@gmail.com>
2023-08-18 17:21:16 +0900

    testrun

--- anaylsis.py
+++ anaylsis.py
@@ -2,43 +2,53 @@ import numpy as np
 import pandas as pd
 from astropy.time import Time
 import matplotlib.pyplot as plt
+from correlation import *
 
-
-stock_symbol = "KOSPI"
+stock_symbol = "S&P500"
 stock_list = pd.read_parquet("./dataset/stock/%s/stock_list.parquet"%stock_symbol)
 
-
-from scipy.stats import ks_2samp
-def d_ks(x, window_size):
-    ref = x[:window_size]
-    res = []
-    for i in range(len(x)-window_size):
-        inf = x[i:i+window_size]
-        d, _ = ks_2samp(ref/ref.max(), inf/inf.max())
-        res.append(d)
-    return np.array(res)
+ref = pd.read_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, "AAPL"))
+date_ref = Time(ref.index.to_numpy()).mjd
+cls_ref = ref.Close.to_numpy()
+window_sizes = {
+            "1w" : 5, "2w" : 10, "1m":20,
+            "3m" : 60, "6m" : 120, "1y": 5*52,
+            "2y" : 5*52*2}
 
 
-for row in range(stock_list.shape[0]):
-    company = stock_list.iloc[row, :]
-    df = pd.read_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, company.Code))
-    close = df.Close.to_numpy()
-    date = Time(df.index.to_numpy()).mjd
+
+
+for i in range(1):#stock_list.shape[0]):
+    company = stock_list.iloc[i, :]
+    try:
+        inf = pd.read_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, company.Symbol))
+    except FileNotFoundError:
+        continue
+    if inf.shape[0]<1:
+        continue
+
+    date_inf = Time(inf.index.to_numpy()).mjd
+    cls_inf = inf.Close.to_numpy()
+
+    x, y1, _, y2 = masking(date_ref, cls_ref, date_inf, cls_inf)
+
+    for lb, ws in window_sizes.items():
+        ks = get_ks(cls_ref, ws)
+        pearson = get_pearson(y1, y2, ws)
+        t, ks_, _, pearson = masking(date_ref[ws:], ks, x[ws:], pearson)
+        
+        spearman = get_spearman(y1, y2, ws)
+        t, ks_, _, spearman = masking(date_ref[ws:], ks, x[ws:], spearman)
+
+        pm = np.average(pearson, weights=ks_)
+        sm = np.average(spearman, weights=ks_)
+        print(lb, pm, sm)
+    
+
+
+
+
 
     
 
-    plt.figure()
-    ax = plt.subplot()
-    axt = plt.twinx(ax)
-    ax.plot(date, close, color="black")
-    for ws in [105, 210, 420]:
-        d = d_ks(close, ws)
-        date_ = date[ws:]
-        axt.plot(date_, d, label=ws)
-    axt.legend(loc='best')
-    ax.set_xticks([date.min()+365*i for i in range(int((date.max()-date.min())/365)+1)])
-    ax.set_xlim(date.min(), date.max())
-    ax.grid(axis="x")
-    plt.show()
-    break
 
--- data_loader.py
+++ data_loader.py
@@ -4,11 +4,15 @@ from astropy.time import Time
 import os
 
 
-stock_symbol = "KOSPI"
+stock_symbol = "S&P500"
 start_date = "2010-01-01"
 stock_list = fdr.StockListing(stock_symbol)
+print(fdr.StockListing(stock_symbol+"-DELISTING"))
 # stock_list.to_parquet("./dataset/stock/%s/stock_list.parquet"%stock_symbol, index=False)
-for row in range(stock_list.shape[0]):
-    company = stock_list.iloc[row, :]
-    df = fdr.DataReader(company.Code, start=start_date)
-    df.to_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, company.Code))
+# for row in range(stock_list.shape[0]):
+#     company = stock_list.iloc[row, :]
+#     df = fdr.DataReader(company.Symbol, start=start_date)
+#     try:
+#         df.to_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, company.Symbol))
+#     except:
+#         pass
