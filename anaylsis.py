aac84d8ce14cb7a3b3efbc845a40f59afa11da6e
dwlee717 <dwlee717@gmail.com>
2023-08-21 17:29:28 +0900

    renew

--- anaylsis.py
+++ anaylsis.py
@@ -3,52 +3,46 @@ import pandas as pd
 from astropy.time import Time
 import matplotlib.pyplot as plt
 from correlation import *
+from functions import *
 
 stock_symbol = "S&P500"
-stock_list = pd.read_parquet("./dataset/stock/%s/stock_list.parquet"%stock_symbol)
+ref_symbol = "AAPL"
 
-ref = pd.read_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, "AAPL"))
-date_ref = Time(ref.index.to_numpy()).mjd
-cls_ref = ref.Close.to_numpy()
-window_sizes = {
+class LookAlike():
+    def __init__(self, stock_symbol, ref_symbol):
+        self.root_direc = "./dataset/stock/%s/"%stock_symbol
+        ref = pd.read_parquet(self.root_direc+"/%s.parquet"%ref_symbol)
+        date_ref = Time(ref.index.to_numpy()).mjd
+        cls_ref = ref.Close.to_numpy()
+        self.x1 = (date_ref, cls_ref)
+        self.ws = {
             "1w" : 5, "2w" : 10, "1m":20,
             "3m" : 60, "6m" : 120, "1y": 5*52,
             "2y" : 5*52*2}
+        self.stock_list = pd.read_parquet(self.root_direc+"/stock_list.parquet")
 
+    def reset(self, ref_symbol):
+        ref = pd.read_parquet(self.root_direc+"/%s.parquet"%ref_symbol)
+        date_ref = Time(ref.index.to_numpy()).mjd
+        cls_ref = ref.Close.to_numpy()
+        self.x1 = (date_ref, cls_ref)
+    
+    def run(self):
+        symbol_list = self.stock_list.Symbol.to_numpy()
+        for i, symbol_inf in enumerate(symbol_list):
+            try:
+                inf = pd.read_parquet(self.root_direc + "%s.parquet"%symbol_inf)
+                x2 = (Time(inf.index.to_numpy()).mjd, inf.Close.to_numpy())
+            except:
+                continue
+            for j, ws in enumerate(self.ws):
+                xy1 = (self.x1[0][:-ws], self.x1[1][:-ws])
+                for k in range(len(x2[0])-ws):
+                    xy2 = (x2[0][k:k+ws], x2[1][k:k+ws])
+                    nxy1, nxy2 = preprocess(xy1, xy2)
+                    c2s = 
 
-
-
-for i in range(1):#stock_list.shape[0]):
-    company = stock_list.iloc[i, :]
-    try:
-        inf = pd.read_parquet("./dataset/stock/%s/%s.parquet"%(stock_symbol, company.Symbol))
-    except FileNotFoundError:
-        continue
-    if inf.shape[0]<1:
-        continue
-
-    date_inf = Time(inf.index.to_numpy()).mjd
-    cls_inf = inf.Close.to_numpy()
-
-    x, y1, _, y2 = masking(date_ref, cls_ref, date_inf, cls_inf)
-
-    for lb, ws in window_sizes.items():
-        ks = get_ks(cls_ref, ws)
-        pearson = get_pearson(y1, y2, ws)
-        t, ks_, _, pearson = masking(date_ref[ws:], ks, x[ws:], pearson)
         
-        spearman = get_spearman(y1, y2, ws)
-        t, ks_, _, spearman = masking(date_ref[ws:], ks, x[ws:], spearman)
 
-        pm = np.average(pearson, weights=ks_)
-        sm = np.average(spearman, weights=ks_)
-        print(lb, pm, sm)
-    
-
-
-
-
-
-    
-
-
+lal = LookAlike(stock_symbol=stock_symbol, ref_symbol=ref_symbol)
+lal.run()
